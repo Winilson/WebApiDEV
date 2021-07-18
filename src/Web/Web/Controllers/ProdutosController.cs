075e5a6cc760d6315bb287d0bc718b1539312943
Winilson <57548741+Winilson@users.noreply.github.com>
2021-07-18 15:27:46 -0300

    Atualização

---
+++ src/Web/Web/Controllers/ProdutosController.cs
@@ -0,0 +1,131 @@
+﻿using AutoMapper;
+using Business.Interfaces;
+using Business.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    [Route("api/produtos")]
+    [ApiController]
+    public class ProdutosController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="notificador"></param>
+        /// <param name="produtoRepository"></param>
+        /// <param name="produtoService"></param>
+        /// <param name="mapper"></param>
+        public ProdutosController(INotificador notificador,
+                                  IProdutoRepository produtoRepository,
+                                  IProdutoService produtoService,
+                                  IMapper mapper) : base(notificador)
+        {
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+            _mapper = mapper;
+        }
+        /// <summary>
+        /// Exibe todos os produtos
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterProdutosFornecedores());
+        }
+        /// <summary>
+        /// Exibe produto por id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProdutoViewModel>> ObterPorId(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel == null) return NotFound();
+
+            return produtoViewModel;
+        }
+
+        /// <summary>
+        /// Adiciona produtos
+        /// </summary>
+        /// <param name="produtoViewModel"></param>
+        /// <returns></returns>
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoViewModel>> Adicionar(ProdutoViewModel produtoViewModel)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var imagemNome = Guid.NewGuid() + "_" + produtoViewModel.Imagem;
+            if (!UploadArquivo(produtoViewModel.ImagemUpload, imagemNome))
+            {
+                return CustomResponse();
+            }
+
+            produtoViewModel.Imagem = imagemNome;
+
+            await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+
+            return CustomResponse(produtoViewModel);
+        }
+        /// <summary>
+        /// Exclui os produtos
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Excluir(Guid id)
+        {
+            var produto = await ObterProduto(id);
+
+            if (produto == null) return NotFound();
+            await _produtoService.Remover(id);
+
+            return CustomResponse(produto);
+        }
+
+        private async Task<ProdutoViewModel> ObterProduto(Guid id)
+        {
+            return _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterProdutoFornecedor(id));
+        }
+
+        private bool UploadArquivo(string arquivo, string imgNome)
+        {
+            var imageDataByteArray = Convert.FromBase64String(arquivo);
+
+            if (string.IsNullOrEmpty(arquivo))
+            {
+                NotificarErro("Forneça uma imagem oara este produto");
+                return false;
+            }
+
+            var filepath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgNome);
+
+            if (System.IO.File.Exists(filepath))
+            {
+                NotificarErro("Já existe um arquivo com este nome");
+                return false;
+            }
+
+            System.IO.File.WriteAllBytes(filepath, imageDataByteArray);
+            return true;
+        }
+
+    }
+}
